e2705d61a9b57df3c98432fcc62f6f7a82ec601f
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-15 15:44:44 +0700

    add Home SignUp and SignIn component

---
+++ src/pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SignIn extends React.Component {
+  render () {
+    return (
+      <div>
+        <h3>SignIn</h3>
+      </div>
+    )
+  }
+}
+
+export default SignIn
---
+++ src/pages/SignUp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import FormSignUp from '../components/FormSignUp'
+import { Link } from 'react-router-dom'
+
+class SignUp extends React.Component {
+  render () {
+    return (
+      <div className='row justify-content-center my-5'>
+        <div className='card mx-5'>
+          <header className='card-header py-0'>
+            <h5 className='card-title my-2'>Sign up</h5>
+          </header>
+          <article className='card-body px-3'>
+            <FormSignUp />
+          </article>
+          <footer className='border-top card-body text-center py-2'>Have an account?<Link to='/signin'>Log In</Link></footer>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default SignUp
