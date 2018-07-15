ebee5bb8d00dfbb14e6fe64fe3ba6ecdf50c3f2d
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-15 15:45:41 +0700

    add use react router and handle path route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './assets/styles/App.css'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/signin' component={SignIn} />
+        </Switch>
       </div>
     )
   }
