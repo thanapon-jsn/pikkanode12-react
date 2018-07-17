e4e5d597f5a1533507e357720e1f06cc4182e25a
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-18 00:21:56 +0700

    update ui NotFound component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import pictureNotFound from '../assets/images/imgAnimal.png'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
@@ -6,10 +7,12 @@ const NotFound = () => {
     <div className='container'>
       <div className='row'>
         <div className='col-md-12'>
-          <div className='card text-center my-5'>
-            <div className='card-body'>
-              <h5 className='card-title'>404 Page Not Found</h5>
-              <p className='card-text'>With supporting text below as a natural lead-in to additional content.</p>
+          <div className='my-5'>
+            <div className='d-flex flex-column'>
+              <h1 className='text-center card-title mr-5'>404 Page Not Found</h1>
+              <p className='card-text'><img className='float-right mr-5' src={pictureNotFound} width='40%' /></p>
+            </div>
+            <div className='d-flex justify-content-center'>
               <Link to='/' className='btn btn-dark'>Back to home</Link>
             </div>
           </div>
