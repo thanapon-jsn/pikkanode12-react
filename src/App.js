e36b92fa3df624f9dd68139b0ce8a0d1c45ae279
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-18 00:24:20 +0700

    add render ui case route not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/styles/App.css'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import NotFound from './pages/NotFound'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
           <Route path='/' exact component={Home} />
           <Route path='/signup' component={SignUp} />
           <Route path='/signin' component={SignIn} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     )
